8a56b179b7e1d7eef85fb15c54d823214c9f9fa3
JaspreetSumal <jaspreetsingh25642@gmail.com>
2025-02-18 15:16:32 -0500

    update

---
+++ app/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Frontend is running'); // Check for this message in the browser console
+});
---
+++ app/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,  // Add this rule
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+  ],
+};
